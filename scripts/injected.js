7ddbd90d752d155a6c2f9079ac4544c341ae7ea8
Gerard Baholli <gerardbaholli@live.it>
2021-03-26 23:51:29 +0100

    add new style

--- scripts/injected.js
+++
@@ -1,5 +0,0 @@
-Element.prototype._attachShadow = Element.prototype.attachShadow;
-Element.prototype.attachShadow = function () {
-  const shadow = this._attachShadow( { mode: "open" } );
-  return shadow;
-};
